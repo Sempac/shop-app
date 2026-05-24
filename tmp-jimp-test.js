4b162cbc50df0328c87393187ed59fceb1c570a1
Sempac <k1979b0@gmail.com>
2026-05-24 19:20:23 +0200

    jimp full methods

--- tmp-jimp-test.js
+++ tmp-jimp-test.js
@@ -1,10 +1,4 @@
-const j=require('jimp');
-const {Jimp}=j;
-console.log('jimp version:', require('./node_modules/jimp/package.json').version);
-console.log('Jimp type:', typeof Jimp);
-// Static methods
-const staticMethods=Object.getOwnPropertyNames(Jimp).filter(k=>typeof Jimp[k]==='function');
-console.log('Jimp static methods:', staticMethods.join(', '));
-// Instance methods (from prototype)
-const protoMethods=Object.getOwnPropertyNames(Jimp.prototype).filter(k=>k!=='constructor');
-console.log('Jimp prototype methods (first 20):', protoMethods.slice(0,20).join(', '));
+const {Jimp}=require('jimp');
+// Liste complète des méthodes de l'instance
+const all=Object.getOwnPropertyNames(Jimp.prototype).filter(k=>k!=='constructor');
+console.log('ALL prototype methods:\n', all.join('\n'));
