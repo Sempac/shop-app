254a9f2529afdc351ceb2ffb27338f33a4d883e8
Sempac <k1979b0@gmail.com>
2026-06-15 01:41:53 +0200

    qr-vitrine: QR code généré côté serveur et embarqué dans le HTML

--- server.js
+++ server.js
@@ -2543,8 +2543,17 @@ app.get('/api/catalogue', async(req,res)=>{
 app.get('/catalogue', (req,res) => {
   res.sendFile(path.join(__dirname,'catalogue.html'));
 });
-app.get('/qr-vitrine', (req,res) => {
-  res.sendFile(path.join(__dirname,'qr-vitrine.html'));
+app.get('/qr-vitrine', async(req,res) => {
+  try {
+    const QRCode = require('qrcode');
+    const s = await pool.query("SELECT value FROM catalogue_settings WHERE key='catalogue_url'");
+    const url = s.rows[0]?.value || 'https://www.thesmartphone.pro/catalogue';
+    const svg = await QRCode.toString(url, {type:'svg', margin:1, width:200});
+    const html = require('fs').readFileSync(path.join(__dirname,'qr-vitrine.html'),'utf8');
+    res.send(html.replace('__QR_SVG__', svg));
+  } catch(e) {
+    res.sendFile(path.join(__dirname,'qr-vitrine.html'));
+  }
 });
 
 // Données catalogue (public)
